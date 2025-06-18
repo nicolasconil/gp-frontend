6da001fafff984d8cadfc9694d6b9ce74ecf8b99
nicolasconil <conilnicolas1@gmail.com>
2025-06-18 19:35:56 -0300

    feat: add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header.jsx';
+import HomePage from './pages/HomePage.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,52 @@
+import { Box, useMediaQuery } from "@mui/material";
+
+const HomePage = () => {
+  const isMobile = useMediaQuery("(max-width: 900px)");
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        width: "100%",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      {!isMobile && (
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+        >
+          <source src="/video.mp4" type="video/mp4" />
+          Tu navegador no soporta la etiqueta de video.
+        </video>
+      )}
+      {isMobile && (
+        <img
+          src="/gp.svg" 
+          alt="GP Footwear"
+          style={{
+            position: "absolute",
+            top: '25%',
+            left: 0,
+            width: "100%",
+            height: "100%",
+            objectFit: "contain", 
+            transform: 'translateY(-50%)'
+          }}
+        />
+      )}
+    </Box>
+  );
+};
+
+export default HomePage;
