b59d9146c9016dd3f58bcea8665dbc66f280ea0d
nicolasconil <conilnicolas1@gmail.com>
2025-12-29 14:45:09 -0300

    fix: last changes deleted

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,61 +3,52 @@ import ProductPage from "./ProductPage.jsx";
 
 const HomePage = () => {
   const isMobile = useMediaQuery("(max-width: 900px)");
-
   return (
     <>
-      <Box
-        sx={{
-          position: "relative",
-          width: "100%",
-          height: isMobile ? "60vh" : "100vh",
-          overflow: "hidden",
-          px: 0,
-          mx: 0,
-        }}
-      >
-        {!isMobile && (
-          <video
-            autoPlay
-            muted
-            loop
-            playsInline
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-              display: "block",
-            }}
-          >
-            <source src="/video.mp4" type="video/mp4" />
-            Tu navegador no soporta la etiqueta de video.
-          </video>
-        )}
-
-        {isMobile && (
-          <img
-            src="/celu.jpg"
-            alt="GP Footwear"
-            style={{
-              position: "absolute",
-              top: 0,
-              left: "50%",
-              width: "100vw",
-              height: "100%",
-              objectFit: "cover",
-              transform: "translateX(-50%)",
-              display: "block",
-            }}
-          />
-        )}
-      </Box>
-
-      <Box sx={{ mt: isMobile ? 1 : 6 }}>
-        <ProductPage />
-      </Box>
+    <Box
+      sx={{
+        position: "relative",
+        width: "100%",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      {!isMobile && (
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+        >
+          <source src="/video.mp4" type="video/mp4" />
+          Tu navegador no soporta la etiqueta de video.
+        </video>
+      )}
+      {isMobile && (
+        <img
+          src="/celu.jpg"
+          alt="GP Footwear"
+          style={{
+            position: "absolute",
+            top: '25%',
+            left: 0,
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            transform: 'translateY(-50%)'
+          }}
+        />
+      )}
+    </Box>
+    <ProductPage />
     </>
   );
 };
