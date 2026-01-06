2614fae82cc74acbbdd594bcfda3b74d5b463da5
nicolasconil <conilnicolas1@gmail.com>
2026-01-06 14:38:19 -0300

    fix: new dimension for box in productpage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -123,7 +123,7 @@ const ProductPage = () => {
   };
 
   return (
-    <Box sx={{ textAlign: 'center', marginTop: { xs: '-375px', sm: '0' }, borderTop: '1px solid #e0e0e0', width: '100%', mt: '150px' }}>
+    <Box sx={{ textAlign: 'center', marginTop: { xs: '-350px', sm: '0' }, borderTop: '1px solid #e0e0e0', width: '100%', mt: '150px' }}>
       <Box
         sx={{
           display: 'inline-block',
