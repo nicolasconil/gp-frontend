4958d07819f13f1dcfe9587eb694adc5459b5aa8
nicolasconil <conilnicolas1@gmail.com>
2026-01-06 13:57:24 -0300

    feat: add decorative lines in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,8 +13,8 @@ const HomePage = () => {
       try {
         v.currentTime = 0.05;
         const p = v.play();
-        if (p && p.catch) p.catch(() => {});
-      } catch (e) {}
+        if (p && p.catch) p.catch(() => { });
+      } catch (e) { }
     };
     v.addEventListener("ended", handleEnded);
     return () => v.removeEventListener("ended", handleEnded);
@@ -56,14 +56,14 @@ const HomePage = () => {
               sx={{
                 position: "relative",
                 width: "100%",
-                maxWidth: 520, 
+                maxWidth: 520,
                 margin: "12px auto",
                 zIndex: 1,
               }}
             >
               <Box
                 sx={{
-                  border: "2.5px solid #e0e0e0",
+                  border: "2.5px solid black",
                   borderRadius: 1.5,
                   overflow: "hidden",
                   boxSizing: "border-box",
@@ -88,6 +88,35 @@ const HomePage = () => {
                   Tu navegador no soporta la etiqueta de video.
                 </video>
               </Box>
+
+              <Box
+                sx={{
+                  position: "absolute",
+                  bottom: -7, 
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                  width: "100%", 
+                  height: "6px",
+                  backgroundColor: "black",
+                  borderRadius: "4px",
+                  zIndex: 20,
+                  pointerEvents: "none",
+                }}
+              />
+
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: 8,
+                  right: -4, 
+                  width: "6px",
+                  height: "calc(100% - 9px)", 
+                  backgroundColor: "black",
+                  borderRadius: "4px",
+                  zIndex: 20,
+                  pointerEvents: "none",
+                }}
+              />
             </Box>
           </>
         )}
